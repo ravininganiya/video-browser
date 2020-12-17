f0cc9cbe3335b0984ba023c66edf36493d66189c
ravininganiya <ry4raviyadav@gmail.com>
2020-12-17 20:47:44 +0530

    Updates

---
+++ precache-manifest.2c3482def7bb69fd3ed972c4acd51c9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a737ffd51dd141fa2988",
+    "url": "/Youtube-api-react-app/static/css/main.deae3210.chunk.css"
+  },
+  {
+    "revision": "a737ffd51dd141fa2988",
+    "url": "/Youtube-api-react-app/static/js/main.a737ffd5.chunk.js"
+  },
+  {
+    "revision": "7a3cdc7243971fb42e95",
+    "url": "/Youtube-api-react-app/static/js/1.7a3cdc72.chunk.js"
+  },
+  {
+    "revision": "6cf8ae799bad99558dfb",
+    "url": "/Youtube-api-react-app/static/js/runtime~main.6cf8ae79.js"
+  },
+  {
+    "revision": "dbcaafb5b3dae7d2f6a5dc8f83399425",
+    "url": "/Youtube-api-react-app/index.html"
+  }
+];
